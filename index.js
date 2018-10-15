c474ea78eea4b9253ca91dfaef954fb4c218c7d0
Jody Neckles <jody@technative.tv>
2018-10-15 09:59:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,12 +17,3 @@ function divide(a, b) {
 function inc(n) {
   return n += 1
 }
-
-/*
-  it('inc(n) increments n and returns the result', function() {
-    expect(inc(a)).toEqual(a + 1)
-  })
-
-  it('dec(n) decrements n and returns the result', function() {
-    expect(dec(a)).toEqual(a - 1)
-  })
