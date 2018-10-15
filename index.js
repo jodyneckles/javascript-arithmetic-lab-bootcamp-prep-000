1ba0c81f60a88aadea8c7364a9a02cf791e776c0
Jody Neckles <jody@technative.tv>
2018-10-15 09:46:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b
 }
+
+function inc(n) {
+  return n == (a + 1)
+}
+
+/*
+  it('inc(n) increments n and returns the result', function() {
+    expect(inc(a)).toEqual(a + 1)
+  })
+
+  it('dec(n) decrements n and returns the result', function() {
+    expect(dec(a)).toEqual(a - 1)
+  })
